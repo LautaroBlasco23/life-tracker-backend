79c24fea43d7973edd75d414b679b8efaacc1162
lautaroblasco23 <lautaroblasco98@gmail.com>
2025-10-20 10:33:34 -0300

    added image related routes to user router

--- internal/domain/user/routes/userRoutes.go
+++ internal/domain/user/routes/userRoutes.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"life-tracker-backend/internal/domain/user/controller"
 	"life-tracker-backend/internal/domain/user/service"
+	"life-tracker-backend/internal/infrastructure/imagestore"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func RegisterUserRoutes(router *gin.RouterGroup, db *gorm.DB) {
-	userService := service.NewUserService(db)
+func RegisterUserRoutes(router *gin.RouterGroup, db *gorm.DB, imageClient *imagestore.Client) {
+	userService := service.NewUserService(db, imageClient)
 	userController := controller.NewUserController(userService)
 
 	userGroup := router.Group("/users")
@@ -19,5 +20,9 @@ func RegisterUserRoutes(router *gin.RouterGroup, db *gorm.DB) {
 		userGroup.GET("", userController.GetAllUsers)
 		userGroup.GET("/:id", userController.GetUserByID)
 		userGroup.DELETE("/:id", userController.DeleteUser)
+
+		// Image related routes
+		userGroup.POST("/profile/image", userController.UploadProfileImage)
+		userGroup.DELETE("/profile/image", userController.DeleteProfileImage)
 	}
 }
