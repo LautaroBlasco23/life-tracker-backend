78098e87e38a5c6c2a9290a9c97eaee0462cc1a0
lautaroblasco23 <lautaroblasco98@gmail.com>
2025-12-26 10:46:35 -0300

    added timezone attribute to user's dto

--- internal/domain/user/dto/userDto.go
+++ internal/domain/user/dto/userDto.go
@@ -3,17 +3,19 @@ package dto
 import "time"
 
 type CreateUserRequest struct {
-	FirstName     string `json:"firstName" binding:"required,min=2,max=50"`
-	LastName      string `json:"lastName" binding:"required,min=2,max=50"`
-	Email         string `json:"email" binding:"required,email"`
-	Password      string `json:"password" binding:"required,min=6"`
-	ProfilePicURL string `json:"profilePicUrl,omitempty"`
+	FirstName     string  `json:"firstName" binding:"required,min=2,max=50"`
+	LastName      string  `json:"lastName" binding:"required,min=2,max=50"`
+	Email         string  `json:"email" binding:"required,email"`
+	Password      string  `json:"password" binding:"required,min=6"`
+	ProfilePicURL string  `json:"profilePicUrl,omitempty"`
+	Timezone      *string `json:"timezone,omitempty"`
 }
 
 type UpdateUserRequest struct {
 	FirstName     *string `json:"firstName,omitempty" binding:"omitempty,min=2,max=50"`
 	LastName      *string `json:"lastName,omitempty" binding:"omitempty,min=2,max=50"`
 	ProfilePicURL *string `json:"profilePicUrl,omitempty"`
+	Timezone      *string `json:"timezone,omitempty" binding:"omitempty,timezone"`
 }
 
 type UserResponse struct {
@@ -21,6 +23,7 @@ type UserResponse struct {
 	UpdatedAt     time.Time `json:"updatedAt"`
 	ProfilePicURL *string   `json:"profilePicUrl,omitempty"`
 	ThumbnailURL  *string   `json:"thumbnailUrl,omitempty"`
+	Timezone      *string   `json:"timezone,omitempty"`
 	FirstName     string    `json:"firstName"`
 	LastName      string    `json:"lastName"`
 	Email         string    `json:"email"`
